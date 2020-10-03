21f6f93c51b99e3c881948fb7ffb6e77d75f964a
TheBreadBro <70241327+TheBreadBro@users.noreply.github.com>
2020-10-03 13:34:31 -0500

    methods updated for userinput.dart
    Tommy

--- vandyhacks2020/lib/userinput.dart
+++ vandyhacks2020/lib/userinput.dart
@@ -1,51 +1,55 @@
+import 'dart:ffi';
 import 'dart:math';
 
 void main() {
-
+  UserInput user = new UserInput([]);
+  user.changeExpense('Rent', 1000);
+  print(user.getExpenses());
 }
 
 class UserInput {
-  var expenses = {'Rent': 0.0, 'Utility': 0.0, 'Subscriptions': 0.0, 'Phone': 0.0, 'Savings': 0.0};
-  var income = new List();
 
-  void addExpense(String expense,double amountExpended){
-    if(!expenses.containsKey(expense)){
-      expenses[expense] = amountExpended;
-    }
-    else{
-      print("Expense already exists");
-    }
+  static const defaultExpenses = {'Rent': 0.0, 'Utility': 0.0, 'Subscriptions': 0.0, 'Bills': 0.0, 'Savings': 0.0, 'Other': 0.0};
+  var expenses = defaultExpenses;
+  var income;
 
+  UserInput(List income){
+    this.income = income;
   }
-  void removeExpense(String expense){
+
+  void clearExpense(String expense){
     if (expenses.containsKey(expense)) {
-        expenses.remove(expense);
-      }
+      expenses[expense] = 0;
+    }
     else
       print("Expense doesn't exist");
   }
+
   void changeExpense(String expense, double changedValue){
     if(expenses.containsKey(expense)){
       expenses[expense] = changedValue;
     }
   }
+
   void addIncome(double amountEarned, {String income = 'Bi-Weekly'}){
-      if(income == 'Bi-Weekly'){
-        this.income.add(amountEarned);
-      }
-      else if(income == 'Weekly'){
-        this.income.add(amountEarned*2);
-      }
-      else if(income == 'Monthly'){
-        this.income.add(amountEarned/2);
-      }
-      else if(income == 'Misc'){
-        this.income.add(amountEarned);
-      }
+    if(income == 'Bi-Weekly'){
+      this.income.add(amountEarned*2);
+    }
+    else if(income == 'Weekly'){
+      this.income.add(amountEarned*4);
+    }
+    else if(income == 'Monthly'){
+      this.income.add(amountEarned);
+    }
+    else if(income == 'Misc'){
+      this.income.add(amountEarned);
+    }
   }
+
   void resetIncome(){
     var income = new List();
   }
+
   Map getExpenses(){
     return(expenses);
   }
