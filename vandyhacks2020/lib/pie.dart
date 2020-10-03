51f28294dbb778b4a849ec996a2894a0b89ea2e6
lyang-86 <53009512+lyang-86@users.noreply.github.com>
2020-10-03 03:41:33 -0500

    Added pie chart template

---
+++ vandyhacks2020/lib/pie.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+// Sets a platform override for desktop to avoid exceptions. See
+// https://flutter.dev/desktop#target-platform-override for more info.
+
+void main() {
+  enablePlatformOverrideForDesktop();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pie Chart Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        brightness: Brightness.dark,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+enum LegendShape { Circle, Rectangle }
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Map<String, double> dataMap = {
+    "Flutter": 5,
+    "React": 3,
+    "Xamarin": 2,
+    "Ionic": 2,
+  };
+  List<Color> colorList = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+  ];
+
+  ChartType _chartType = ChartType.ring;
+  bool _showCenterText = true;
+  double _ringStrokeWidth = 32;
+  double _chartLegendSpacing = 32;
+
+  bool _showLegendsInRow = false;
+  bool _showLegends = true;
+
+  bool _showChartValueBackground = true;
+  bool _showChartValues = true;
+  bool _showChartValuesInPercentage = false;
+  bool _showChartValuesOutside = false;
+
+  LegendShape _legendShape = LegendShape.Circle;
+  LegendPosition _legendPosition = LegendPosition.right;
+
+  int key = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final chart = PieChart(
+      key: ValueKey(key),
+      dataMap: dataMap,
+      animationDuration: Duration(milliseconds: 800),
+      chartLegendSpacing: _chartLegendSpacing,
+      chartRadius: MediaQuery.of(context).size.width / 3.2 > 300
+          ? 300
+          : MediaQuery.of(context).size.width / 3.2,
+      colorList: colorList,
+      initialAngleInDegree: 0,
+      chartType: _chartType,
+      centerText: _showCenterText ? "HYBRID" : null,
+      legendOptions: LegendOptions(
+        showLegendsInRow: _showLegendsInRow,
+        legendPosition: _legendPosition,
+        showLegends: _showLegends,
+        legendShape: _legendShape == LegendShape.Circle
+            ? BoxShape.circle
+            : BoxShape.rectangle,
+        legendTextStyle: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      chartValuesOptions: ChartValuesOptions(
+        showChartValueBackground: _showChartValueBackground,
+        showChartValues: _showChartValues,
+        showChartValuesInPercentage: _showChartValuesInPercentage,
+        showChartValuesOutside: _showChartValuesOutside,
+      ),
+      ringStrokeWidth: _ringStrokeWidth,
+    );
